e7675cf45c1514ce01792537c954b5cbcfce01cb
Christopher Yamas <yamas.chris@gmail.com>
2022-03-17 19:57:20 -0500

    Header component create

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Horns, a Gallery.</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
