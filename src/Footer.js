e9cb0fdf0c1fd07642fe6c64df93c857b4ac58c7
Christopher Yamas <yamas.chris@gmail.com>
2022-03-17 19:58:03 -0500

    Footer component create

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>&copy; Christopher Yamas</footer>
+    )
+  }
+}
+
+export default Footer;
