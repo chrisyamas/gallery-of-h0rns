1a18b5e7b70a9fbb60c15e0b3932b43497b7ade9
Christopher Yamas <yamas.chris@gmail.com>
2022-03-18 14:51:14 -0500

    modify to pass props to eacH HornedBeast component

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import Rataxes from './rataxes.jpg';
-import Charuni from './charuni.png';
+
 
 class Main extends React.Component {
   render() {
+    let beasts=this.props.data.map((beast,idx) => {
+      return (
+        <HornedBeast
+        title = {beast.title}
+        imageUrl = {beast.image_url}
+        keyword = {beast.keyword}
+        description = {beast.description}
+        key = {idx}
+        />
+      );
+    });
     return (
-      <>
-        <HornedBeast
-        title="Lord Rataxes"
-        imageUrl={Rataxes}
-        description="Monarch de Jure of Rhinoland kingdom in fictional Babar universe."
-        keyword="Picture of Lord Rataxes looking mischevious"/>
-        <HornedBeast
-        title="Charlie The Unicorn"
-        imageUrl={Charuni}
-        description="Tragic eponymous character of disturbing 2005 Flash animated short film."
-        keyword="Picture of Charlie The Unicorn looking irritated"/>
-      </>
-    )
-  }
-}
+      <main>
+        {beasts}
+      </main>
+    );
+  };
+};
+
 
 export default Main;
