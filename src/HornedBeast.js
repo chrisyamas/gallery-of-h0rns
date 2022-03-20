c044ddc5232d74b72bc4db01a9243fc46a03f7d7
Christopher Yamas <yamas.chris@gmail.com>
2022-03-20 17:38:06 -0500

    state add, styling import

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      favorites: 0
+    }
+  }
+  handleFavorites = () => {
+    this.setState({favorites: this.state.favorites + 1})
+  };
+
   render() {
     return (
-      <>
+      <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.keyword}/>
+        <img 
+        src={this.props.imageUrl}
+        alt={this.props.keyword}/>
         <p>{this.props.description}</p>
-      </>
-    )
-  }
-}
+        <p>{this.state.favorites} favorites</p>
+        <p onClick={this.handleFavorites}>Click to Favorite this beast!</p>
+      </article>
+    );
+  };
+};
 
 export default HornedBeast;
